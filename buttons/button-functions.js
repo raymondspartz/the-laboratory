de311fa3e8daec61d553fa991260c689bc693ba2
Raymond Spartz <raymond.luke.spartz@gmail.com>
2021-11-29 13:24:01 -0600

    new-button

--- buttons/button-functions.js
+++ buttons/button-functions.js
@@ -1,18 +1,21 @@
 // allow functionality of a button
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
+
 
 //add to the list
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function stored as variable to handle button interactivity
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+  };
 
 // call createTaskHandler function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
